6f47156f9976ba4ca48b1cfc72c5396d84731c06
betocostadev <robertomarx@gmail.com>
2019-06-25 12:18:06 -0300

    added some modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,6 +3,8 @@ Introduction to Node.js */
 
 // Node docs: https://nodejs.org/api/
 
+const validator = require('validator');
+
 // Asking for another file (use some modules);
 const getNotes = require('./utils.js');
 
@@ -12,3 +14,21 @@ const getNotes = require('./utils.js');
 
 const notes = getNotes();
 console.log(notes);
+
+/* We added the npm package validator above just to test it. Using it below: */
+const USER_ONE = {
+  email: 'foo@bar.com',
+  website: 'www.betocostadev.github.io'
+};
+
+const checkUserData = user => {
+  let check = false;
+  if (validator.isEmail(user.email)) {
+    console.log('User email is valid');
+  }
+  if (validator.isURL(user.website)) {
+    console.log('User website is valid');
+  }
+}
+
+checkUserData(USER_ONE);
