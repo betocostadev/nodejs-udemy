ece81e6cdadf93682e05d19242e7ee8c90703996
betocostadev <robertomarx@gmail.com>
2019-07-01 18:58:57 -0300

    added some callback examples

---
+++ playground/04-callbacks.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+  console.log(`1 second passed.`)
+}, 1000);
+
+const names = ['Andrew', 'Jen', 'Jess', 'Beto'];
+const shortNames = names.filter((name) => {
+  return name.length <= 4;
+});
+console.log(shortNames);
+
+// Just an example
+const geocode = (address, callback) => {
+  // Simulate a server delay response
+  setTimeout(() => {
+    const data = {
+      latitude: 0,
+      longitude: 0
+    }
+    callback(data)
+  }, 750);
+}
+
+geocode('Philadelphia', (data) => {
+  console.log(data);
+})
+
+const add = (num1, num2, callback) => {
+  setTimeout(() => {
+    const operation = num1 + num2;
+    callback(operation);
+  },1500)
+}
+
+add(1, 4, (sum) => {
+  console.log(sum);
+});
--- weather-app/app.js
+++ weather-app/app.js
@@ -8,14 +8,7 @@ Geolocation -> userAddress -> Lat/long -> Darksky weather
 // request for node.js
 const request = require('request');
 
-// Base url request... api/forecast/key/lat,long
-// const url = 'https://api.darksky.net/forecast/10bdc83894a177fc915d0fb35c56464f/-23.474,-46.643';
-// options as query strings: ?key=value
-const darkskyUrl = 'https://api.darksky.net/forecast/10bdc83894a177fc915d0fb35c56464f/-23.474,-46.643';
-const options = '?units=si'
-const darksky = darkskyUrl + options;
-
-// Mapbox
+// Mapbox API
 const mapboxAPI = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
 const mapboxKey = 'access_token=pk.eyJ1Ijoicm9iZXJ0b2Nvc3RhIiwiYSI6ImNqeGtoendvNzAwYWkzb21vdDBlejhocjYifQ.F9S_9yxCMIatlHCeqqWDfQ';
 const mapboxLocation = 'Sao%20Paulo.json?'
@@ -23,25 +16,47 @@ const mapboxLimit = '&limit=1';
 const mapboxUrl = `${mapboxAPI}${mapboxLocation}${mapboxKey}${mapboxLimit}`;
 
 // Mapbox Request
-
 request({ url: mapboxUrl, json: true}, (error, response) => {
-  // console.log(response.body);
-  const place = response.body.features[0]['place_name'];
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0]
-  console.log(`Location: ${place}. Lat: ${latitude}. Long: ${longitude}`);
+  if (error) {
+    console.log(`Unable to connect to geolocation service!`);
+  } else if (response.body.message) {
+    console.log(`Error: ${response.body.message}`);
+  } else if (response.body.features.length === 0) {
+    console.log(`Unable to found location: ${response.body.query[0]}`);
+  } else {
+    // console.log(response.body);
+    const placeName = response.body.features[0]['place_name'];
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0]
+    console.log(`Location: ${placeName}. Lat: ${latitude}. Long: ${longitude}`);
+  }
 })
+
+
+// Darksky API
+// Base url request... api/forecast/key/lat,long
+// const url = 'https://api.darksky.net/forecast/10bdc83894a177fc915d0fb35c56464f/-23.474,-46.643';
+// options as query strings: ?key=value
+const darkskyUrl = 'https://api.darksky.net/forecast/10bdc83894a177fc915d0fb35c56464f/-23.474,-46.643';
+const options = '?units=si'
+const darksky = darkskyUrl + options;
 
 /* Some options of the request module:
 json - parse data into json
  */
 request({ url: darksky, json: true }, (error, response) => {
-  const data = response.body;
-  const currentTemp = data.currently.temperature;
-  const rainChance = data.currently.precipProbability;
-  const summary = data.daily.data[0].summary;
-  console.log(`Current temperature is ${currentTemp} and the chance of rain is ${rainChance}%`);
-  console.log(summary);
+  if (error) {
+    console.log(`Unable to connect to weather service!`);
+  } else if (response.body.error) {
+    console.log(`Error code: ${response.body.code}. ${response.body.error}`);
+  } else {
+    const data = response.body;
+    const currentTemp = data.currently.temperature;
+    const rainChance = data.currently.precipProbability;
+    const summary = data.daily.data[0].summary;
+    console.log(`Current temperature is ${currentTemp} and the chance of rain is ${rainChance}%`);
+    console.log(summary);
+  }
 });
 
 
