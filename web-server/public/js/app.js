2444588039d8eda32aaafbb303149c8e5e931a02
betocostadev <robertomarx@gmail.com>
2019-07-06 15:12:03 -0300

    added functions to fend

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,37 @@
 console.log(`Client side javascript file is loaded!`)
+
+// We will use the Fetch browser API to get the data we want
+
+/*
+// Fetch example:
+fetch('http://puzzle.mead.io/puzzle').then((response) =>{
+  response.json().then((data) => {
+    console.log(data.puzzle)
+  })
+}) */
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-one');
+const messageTwo = document.querySelector('#message-two');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value
+
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+        messageTwo = ''
+      } else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
+      }
+    })
+  })
+})
